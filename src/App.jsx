d5d5152be0e5134247684b1fb2e15aff62ff901a
Amar <amarkhadkabardiya1234@gmail.com>
2025-11-03 11:45:00 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./css/App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Favorite from "./pages/Favorite";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorite" element={<Favorite />} />
+        </Routes>
+      </main>
+      </MovieProvider>
+   
+  );
+}
+
+export default App;
---
+++ src/contexts/MovieContext.jsx
@@ -0,0 +1,41 @@
+import { createContext,useState, useContext, useEffect } from "react";
+
+const MovieContext = createContext()
+
+export const useMovieContext = () => useContext(MovieContext)
+
+export const MovieProvider = ({children}) => {
+    const [favorites, setFavorite] = useState([])
+
+    useEffect(() =>{
+        const storedFavs = localStorage.getItem("favorites")
+        if (storedFavs) setFavorite(JSON.parse(storedFavs))
+    }, [])
+
+    useEffect(() =>{
+        localStorage.setItem('favorites', JSON.stringify(favorites))
+
+    }, [favorites])
+     
+    const addToFavorite = (movie) =>{
+        setFavorite(prev => [...prev, movie])
+    }
+    const removeFromFavorite = (movieId) =>{
+        setFavorite(prev => prev.filter(movie =>movie.id !== movieId))
+    }
+    const isFavorite = (movieId) =>{
+        return favorites.some(movie => movie.id === movieId)
+    }
+    const value ={
+        favorites,
+        addToFavorite,
+        removeFromFavorite,
+        isFavorite
+    }
+
+    return (<MovieContext.Provider value={value}>
+        {children}
+    </MovieContext.Provider>
+    );
+
+}
