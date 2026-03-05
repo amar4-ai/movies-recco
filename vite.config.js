93607ceaba88d9c15fc608e4d7df835a74e5f54f
Amar <amarkhadkabardiya1234@gmail.com>
2026-03-05 14:10:01 +0545

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base:'/amar4-ai/movies-recco/',
   plugins: [react()],
 })
